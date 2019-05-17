c1d93bd212d0d1d5d3a7f8cc4985b3239dbf3ce1
zhuwutao <1255284280@qq.com>
2019-05-17 15:03:21 +0800

    添加.ignore插件，忽略不提交的文件；springboot-web 自定义filter

---
+++ springboot-web/src/main/java/com/szu/springboot/WebConfiguration.java
@@ -0,0 +1,42 @@
+package com.szu.springboot;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created by zhuwutao on 2019/5/17
+ */
+@Configuration
+public class WebConfiguration {
+
+
+    @Bean
+    public FilterRegistrationBean testFilterRegistration() {
+
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        //将自定义的filter注册到FilterRegistrationBean中
+        registration.setFilter(new MyFilter());
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("paramName", "paramValue");
+        registration.setName("MyFilter");
+        registration.setOrder(1);
+        return registration;
+    }
+
+    public class MyFilter implements Filter{
+
+        @Override
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+                throws IOException, ServletException {
+            HttpServletRequest request = (HttpServletRequest) servletRequest;
+            System.out.println("this is MyFilter,url :"+request.getRequestURI());
+            filterChain.doFilter(servletRequest, servletResponse);
+
+        }
+    }
+}
