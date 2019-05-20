d90428c93ffb4c6dd47e0c44c964505b4da17b50
zhuwutao <1255284280@qq.com>
2019-05-20 17:33:47 +0800

    springboot-mybatis 多数据源

---
+++ springboot-mybatis/src/main/java/com/szu/springboot/mapper/test2/User2Mapper.java
@@ -0,0 +1,37 @@
+package com.szu.springboot.mapper.test2;
+
+
+import com.szu.springboot.enums.UserSexEnum;
+import com.szu.springboot.model.User;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+
+public interface User2Mapper {
+
+
+	@Select("SELECT * FROM users")
+	@Results({
+			@Result(property = "userSex",  column = "user_sex", javaType = UserSexEnum.class),
+			@Result(property = "nickName", column = "nick_name")
+	})
+	List<User> getAll();
+
+	@Select("SELECT * FROM users WHERE id = #{id}")
+	@Results({
+			@Result(property = "userSex",  column = "user_sex", javaType = UserSexEnum.class),
+			@Result(property = "nickName", column = "nick_name")
+	})
+	User getOne(Long id);
+
+	@Insert("INSERT INTO users(userName,passWord,user_sex) VALUES(#{userName}, #{passWord}, #{userSex})")
+	void insert(User user);
+
+	@Update("UPDATE users SET userName=#{userName},nick_name=#{nickName} WHERE id =#{id}")
+	void update(User user);
+
+	@Delete("DELETE FROM users WHERE id =#{id}")
+	void delete(Long id);
+
+}
